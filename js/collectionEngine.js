70bc233137e2643e7dbaa51208afbcba34fe1396
BpavelA <x-man-pb@yandex.ru>
2025-12-12 16:57:04 +0300

    первая карточка готова

--- js/collectionEngine.js
+++ js/collectionEngine.js
@@ -1,5 +1,30 @@
 'use strict';
 
-// логика подгрузки
-// при загрузке html, цикл пробегается по всем txt файлам и считывает первую строку - это будет summary
-// при событии "click" по считывает файл, первая строка - summary, на базе остального input и label, по мени файла генерируется id 
+const data = ["Ich stehe auf dem Standpunkt, dass...",
+ "Meines Erachtens ist das ...",
+ "Für mich steht fest, dass ...",
+ "Ich vertrete die Ansicht, dass ...",
+ "Ich bin der festen Übezeugung, dass ..."];
+
+const summary = document.querySelector('#ma');
+
+data.forEach(phrase =>
+{
+ let index = data.indexOf(phrase);
+ let div = document.createElement('div');
+ let input = document.createElement('input');
+ let label = document.createElement('label');
+ summary.after(div);
+ div.prepend(input);
+ input.after(label);
+ label.innerHTML = phrase;
+ div.classList.add('vocabulare');
+ input.type = 'checkbox';
+ input.id = `ma${index}`;
+ label.setAttribute('for', `ma${index}`);
+});
+
+
+
+
+
